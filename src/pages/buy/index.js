4cc2cf829dfadca20a404ac7b85717bc7dec02e0
Matheus <matheusrodrigofilho@outlook.com>
2021-10-20 00:26:32 -0300

    feat: small changes in screan to make a order

--- src/pages/buy/index.js
+++ src/pages/buy/index.js
@@ -4,11 +4,11 @@ import Footer from '../../globalComponents/footer/Footer.js';
 import Lanche from './components/lanche';
 
 import logoHamburgueria from '../../images/logohamburgueria.png';
-import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
+// import { useParams } from 'react-router';
+import { useState } from 'react';
 
 function Buy() {
-  const { id } = useParams();
+  // const { id } = useParams();
 
   const [itensInCart, setItensInCart] = useState([]);
 
@@ -27,244 +27,263 @@ function Buy() {
   return (
     <>
       <NavBar></NavBar>
-      <div className="content">
-        <div className="lanches">
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 1,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 1,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 2,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 2,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 3,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 3,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 4,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 4,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 5,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 5,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 6,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 6,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 7,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 7,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 8,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 8,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 9,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 9,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 10,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 10,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7,00',
-                id: 11,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Burguer',
-                price: 'R$7.00',
-                id: 11,
-              })
-            }
-            title="X-Burguer"
-            price="R$7.00"
-            image={logoHamburgueria}
-          />
-          <Lanche
-            onClick={() =>
-              handleAddLancheAtCart({
-                name: 'X-Bacon',
-                price: 'R$14,00',
-                id: 12,
-              })
-            }
-            onRemove={() =>
-              handleRemoveLancheAtCart({
-                name: 'X-Bacon',
-                price: 'R$14.00',
-                id: 12,
-              })
-            }
-            title="X-Bacon"
-            price="R$14.00"
-            image={logoHamburgueria}
-          />
+      <div className="contentLanches">
+        <div>
+          <div className="lanches">
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 1,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 1,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 2,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 2,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 3,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 3,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 4,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 4,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 5,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 5,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 6,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 6,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 7,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 7,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 8,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 8,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 9,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 9,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 10,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 10,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7,00',
+                  id: 11,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Burguer',
+                  price: 'R$7.00',
+                  id: 11,
+                })
+              }
+              title="X-Burguer"
+              price="R$7.00"
+              image={logoHamburgueria}
+            />
+            <Lanche
+              onClick={() =>
+                handleAddLancheAtCart({
+                  name: 'X-Bacon',
+                  price: 'R$14,00',
+                  id: 12,
+                })
+              }
+              onRemove={() =>
+                handleRemoveLancheAtCart({
+                  name: 'X-Bacon',
+                  price: 'R$14.00',
+                  id: 12,
+                })
+              }
+              title="X-Bacon"
+              price="R$14.00"
+              image={logoHamburgueria}
+            />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
+            <button
+              onClick={() => console.log(itensInCart)}
+              style={{
+                cursor: 'pointer',
+                backgroundColor: 'transparent',
+                border: '1px solid black',
+                padding: '10px 20px',
+                justifySelf: 'center',
+                alignSelf: 'center',
+                margin: '20px auto',
+                borderRadius: '5px',
+              }}
+            >
+              Concluir Compra
+            </button>
+          </div>
         </div>
-
-        <button
-          onClick={() => console.log(itensInCart)}
-          style={{ cursor: 'pointer' }}
-        >
-          test
-        </button>
+        <div>
+          <h1>Falta criar a parte de criar um lanche customizavel</h1>
+        </div>
       </div>
       <Footer></Footer>
     </>
